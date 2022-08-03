edbc5181e9a2f93281dcfd3d3e70f559d5a5ccba
Shahwaiz Naveed <shahwaiznaveed46@gmail.com>
2022-08-04 00:00:07 +0500

    Home

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.get("/", (req, res) => res.send("Home"));
 app.use("/auth", authRoutes);
 app.use("/admin", userRoutes);
 app.use("/doctor", doctorsRoutes);
